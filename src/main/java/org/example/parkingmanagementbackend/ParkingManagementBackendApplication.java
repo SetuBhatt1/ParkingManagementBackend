fb9fc995b9a8d36669836bcc4654d8ffa356646b
Setu Bhatt <bansetu1@gmail.com>
2024-02-20 22:07:28 +0530

    initial commit

---
+++ src/main/java/org/example/parkingmanagementbackend/ParkingManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.parkingmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingManagementBackendApplication.class, args);
+    }
+
+}
