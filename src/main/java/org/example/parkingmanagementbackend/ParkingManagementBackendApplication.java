12181e4dca16eddd5ed4aa6f2af303b61d3b09af
Setu Bhatt <bansetu1@gmail.com>
2024-02-26 17:59:25 +0530

    configured database successfully, but remaining to add them as env variables

--- src/main/java/org/example/parkingmanagementbackend/ParkingManagementBackendApplication.java
+++ src/main/java/org/example/parkingmanagementbackend/ParkingManagementBackendApplication.java
@@ -1,13 +1,20 @@
 package org.example.parkingmanagementbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class ParkingManagementBackendApplication {
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingManagementBackendApplication.class, args);
     }
-
 }
